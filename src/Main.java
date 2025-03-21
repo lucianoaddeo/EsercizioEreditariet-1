e30956593560d9a9b43eab5d71e64b26ad992b78
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-03-21 12:45:30 +0100

    esercizio metodi stringhe 1 salvato nella repo esercizioEreditarieta1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        Animale a = new Animale();
-        a.faIlVerso();
-        Gatto g = new Gatto();
-        g.faIlVerso();
+        System.out.println(comparaStringhe("Giorgio", "Giorgio"));
+        System.out.println(comparaStringhe("Giorgio", "giorgio"));
+    }
 
+    public static StringBuilder comparaStringhe(String s1, String s2) {
+
+        StringBuilder risultato = new StringBuilder("Le due stringhe sono ");
+
+        if (s1.compareTo(s2) == 0)
+            return risultato.append("uguali");
+        else
+            return risultato.append("differenti");
     }
 }
